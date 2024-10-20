802d85ddcdbba3c62e79b93d1f03afc234d8dd58
Luca Mateo Acosta Servian <lucamateo1110@gmail.com>
2024-10-20 20:34:51 +0200

    Menú Funcional
    Jugar+Opciones+Salir

--- PR01/Assets/Scripts/MenuPrincipal.cs
+++ PR01/Assets/Scripts/MenuPrincipal.cs
@@ -9,4 +9,10 @@ public class MenuPrincipal : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void Salir()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
 }
